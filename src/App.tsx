e80418dffa19d8313039b6759e952387b8520cb2
Calvin Ngugi <ncalvin67@gmail.com>
2023-08-14 16:49:42 +0300

    :sparkles: Scroll to top component created and functional

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import MainContainer from "./components/MainContainer"
+import ScrollToTop from "./components/ScrollToTop"
 
 const App = () => {
   return (
     <div>
       <MainContainer />
+      <ScrollToTop />
     </div>
   )
 }
