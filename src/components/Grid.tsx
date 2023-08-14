abbff160e1402051c1344105737393e89e6a253d
awadh <awadhhassan566@gmail.com>
2023-08-14 19:15:30 +0300

    grid   styling

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
-import grid from "assets/"
-
-function Grid() {
+const Grid = () => {
   return (
-    <div>sd</div>
-  )
-}
+    <div className="container mx-auto">
+      <div className="grid grid-cols-3 gap-2 p-20 bg-yellow-200 lg:grid-rows-2">
+        <div className="w-full rounded">
+          <img src='banner.jpg'alt="banner" className="cover" />
+        </div>
+        <div className="w-full col-span-2 row-span-3 rounded">
+          <img src='manicure.jpg' alt="manicure" className="cover"/>
+        </div>                                  
+        <div className="w-full rounded">
+           <img src='massage.jpg'alt="massage" className="cover" />
+        </div>
+        <div className="w-full rounded">
+          <img src='nails-red.jpg' alt="nailsred" className="cover"/>
+        </div>
+        <div className="w-full rounded">
+          <img src='nails.jpg' alt="nails" className="cover"/>
+        </div>
+        <div className="w-full rounded">
+          <img src='salon.jpg' alt="salon" className="cover"/>
+        </div>
+        <div className="w-full rounded">
+          <img src='salon2.jpg' alt="salon2" className="cover"/>
+        </div>
+        {/* <div className="w-full rounded">
+          <img src='pedicure.jpg' alt="salon" className="cover"/>
+        </div> */}
+      </div>
+    </div>
+  );
+};
 
-export default Grid
+export default Grid;
+                    
