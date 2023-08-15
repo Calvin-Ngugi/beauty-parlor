a0cbfbf777043d65992b9989f1fab0908aec2a3c
Calvin Ngugi <ncalvin67@gmail.com>
2023-08-15 11:28:34 +0300

    :lipstick: :iphone: Redesigining the mobile style

--- src/Components/InfoSection.tsx
+++ src/Components/InfoSection.tsx
@@ -1,8 +1,8 @@
 const InfoSection = () => {
   return (
-    <div className="absolute -bottom-36 right-0 bg-fuchsia-600 w-[45rem] h-[45%]">
-      <div className="pt-20 pe-10 ps-20">
-        <div className="grid grid-cols-3">
+    <div className="relative md:absolute md:-bottom-36 md:right-0 bg-fuchsia-600 md:w-[45rem] w-[100%] h-[45%] text-white">
+      <div className="pt-20 pe-10 ps-20 pb-20">
+        <div className="grid md:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-y-7">
           <div className="flex flex-col">
             <p className="font-bold">Parlour</p>
             <p>Lunar parlour</p>
@@ -15,17 +15,17 @@ const InfoSection = () => {
             <p className="font-bold">Mobile</p>
             <p>+254 701141934</p>
           </div>
-          <div className="flex flex-col md:mt-24">
+          <div className="flex flex-col md:mt-16">
             <p className="font-bold">Address</p>
             <p>00200, Nairobi,</p>
             <p>42429 Kenya</p>
           </div>
-          <div className="flex flex-col md:mt-24">
+          <div className="flex flex-col md:mt-16">
             <p className="font-bold">Services</p>
             <p>Sparking</p>
             <p>SPA Cream</p>
           </div>
-          <div className="flex flex-col md:mt-24">
+          <div className="flex flex-col md:mt-16">
             <p className="font-bold">Working Hours</p>
             <p>Monday to Friday</p>
             <p>0900-1900</p>
--- src/Components/MainContainer.tsx
+++ src/Components/MainContainer.tsx
@@ -1,10 +1,14 @@
 import Header from "./Header";
+import InfoSection from "./InfoSection";
 import Services from "./Services";
 
 const MainContainer = () => {
   return (
     <div>
       <Header />
+      <div className="md:hidden block">
+        <InfoSection />
+      </div>
       <Services />
     </div>
   );
