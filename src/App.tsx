731865bb318aa15539a8fc90dcc620a95ef36a04
awadh <awadhhassan566@gmail.com>
2023-08-16 21:21:50 +0300

    :memo:  contact update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import MainContainer from "./components/MainContainer"
 import ScrollToTop from "./components/ScrollToTop"
+import Contact from "./components/Contact"
 
 const App = () => {
   return (
     <div>
       <MainContainer />
       <ScrollToTop />
+      <Contact />
     </div>
   )
 }
